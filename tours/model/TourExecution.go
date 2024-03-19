935bf5473a824b5ab628aaab2aa7151ccd6125c1
Natalija Popović <popovic.ra117.2020@uns.ac.rs>
2024-03-19 16:46:02 +0100

    fix: Creation of TourExecution

--- tours/model/TourExecution.go
+++ tours/model/TourExecution.go
@@ -1,6 +1,9 @@
 package model
 
 import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -22,11 +25,26 @@ type TourExecution struct {
 	TourId    int64 `json:"toudId"`
 	TouristId int64 `json:"touristId"`
 	Status    TourExecutionStatus
-	Position  Position `gorm:"type:jsonb"`
+	Position  Position `gorm:"type:jsonb;"`
 	Tasks     []PointTask
 }
 
 func (execution *TourExecution) BeforeCreate(scope *gorm.DB) error {
 	execution.Id = int64(uuid.New().ID()) + time.Now().UnixNano()/int64(time.Microsecond)
 	return nil
+}
+
+func (a Position) Value() (driver.Value, error) {
+	return json.Marshal(a)
+}
+func (a *Position) Scan(value interface{}) error {
+	if value == nil {
+		*a = Position{}
+		return nil
+	}
+	b, ok := value.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+	return json.Unmarshal(b, a)
 }
