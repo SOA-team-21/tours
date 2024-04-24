3786055194d7f203f035025eb53949b17db1f7c0
ivanailijin <ilijin.ra104.2020@uns.ac.rs>
2024-04-24 22:43:35 +0200

    Minor change

--- tours/main.go
+++ tours/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func initDB() *gorm.DB {
-	dsn := "user=postgres password=super dbname=soa_tours host=database port=5432 sslmode=disable"
+	dsn := "user=postgres password=super dbname=soa_tours host=tours-database port=5432 sslmode=disable"
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
